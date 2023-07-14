45d4e8bf6014c034c77d6354686591aabcd28778
Justus Braun <57273189+Justus-Braun@users.noreply.github.com>
2023-07-15 00:05:24 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,7 +42,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/YBN-Creative-Marketplace/ybn-creative-marketplace.github.io',
         },
         // blog: {
         //   showReadingTime: true,
